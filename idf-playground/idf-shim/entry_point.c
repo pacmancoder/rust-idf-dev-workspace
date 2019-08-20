5d0a64bf9fa4bef8a8f8f81cdf369a1c831b9ce3
Vladislav Nikonov <pacmancoder@gmail.com>
2019-08-20 22:53:45 +0300

    Prepared worspace for usable idf builds

---
+++ idf-playground/idf-shim/entry_point.c
@@ -0,0 +1,1 @@
+extern void app_main(void);
--- idf-playground/src/lib.rs
+++ idf-playground/src/lib.rs
@@ -1,7 +1,6 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#![no_std]
+
+extern crate panic_halt;
+
+#[no_mangle]
+extern "C" fn app_main() {}
