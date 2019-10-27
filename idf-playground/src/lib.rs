3f28b62597a1e5e79fe2cf8c03c69b13e8fe6b3d
Vladislav Nikonov <pacmancoder@gmail.com>
2019-10-27 20:42:01 +0200

    Implemented basic pwm

--- idf-playground/src/lib.rs
+++ idf-playground/src/lib.rs
@@ -7,8 +7,52 @@ use idf_hal::{
     peripherals::*,
     wifi::*,
     gpio::*,
+    pwm::*,
 };
 use idf_hal::gpio::{GpioHardware, PinInitializer};
+
+enum AppError {
+    Unknown,
+}
+
+impl From<PwmInitializationError> for AppError {
+    fn from(err: PwmInitializationError) -> Self {
+        AppError::Unknown
+    }
+}
+
+impl From<PwmConfigurationError> for AppError {
+    fn from(err: PwmConfigurationError) -> Self {
+        AppError::Unknown
+    }
+}
+
+
+fn init_pwm<C1, C2>(channel1 : C1, channel2: C2) -> Result<Pwm, AppError>
+    where C1: GpioPin + PwmPinMarker, C2: GpioPin + PwmPinMarker
+{
+    let period = 1000 * 500; // 500 ms
+    let duty_channel1 = period / 2;
+    let duty_channel2 = period / 4;
+
+    let phase_channel1 = 30;
+    let phase_channel2 = 60;
+
+    let mut pwm = PwmInitializer::new()
+        .set_period(period)?
+        .add_channel(channel1, duty_channel1)?
+        .add_channel(channel2, duty_channel2)?
+        .initialize().map_err(|(err, _)| err)?;
+
+    pwm.configure(|config| {
+        config
+            .set_phase(0, phase_channel1)?
+            .set_phase(1, phase_channel2)?;
+        Ok(())
+    })?.start();
+
+    Ok(pwm)
+}
 
 #[no_mangle]
 extern "C" fn app_main() {
@@ -30,11 +74,13 @@ extern "C" fn app_main() {
 
     let gpio = GpioHardware::new(peripherals.gpio);
 
-    let mut rled_gpio = PinInitializer::new(gpio.gpio12).configure_as_output().init();
-    let mut gled_gpio = PinInitializer::new(gpio.gpio14).configure_as_output().init();
-    let mut bled_gpio = PinInitializer::new(gpio.gpio13).configure_as_output().init();
+    let mut red_led_gpio = PinInitializer::new(gpio.gpio12).configure_as_output().init();
 
-    rled_gpio.set_level(true);
-    gled_gpio.set_level(true);
-    bled_gpio.set_level(true);
+    if init_pwm(gpio.gpio14, gpio.gpio13).is_err() {
+        red_led_gpio.set_level(true);
+    } else {
+        red_led_gpio.set_level(false);
+    }
+
+    loop {};
 }
